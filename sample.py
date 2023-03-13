6c2b8194916279375aa8a99118b737342d8060ec
Sijun Liu <liusijun1985@gmail.com>
2023-03-13 16:00:46 -0700

    update

--- sample.py
+++ sample.py
@@ -1,5 +1,20 @@
 from google.cloud import kms_v1
 
+import logging
+from http.client import HTTPConnection
+
+def debug_requests_on():
+    '''Switches on logging of the requests module.'''
+    HTTPConnection.debuglevel = 1
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+debug_requests_on()
+
 project = "sijunliu-nondca-test"
 
 client = kms_v1.KeyManagementServiceClient()
